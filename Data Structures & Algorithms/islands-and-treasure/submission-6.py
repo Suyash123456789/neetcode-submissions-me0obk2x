20dc2006e81cf271b53b3adaaaa4655a48f019cb
Suyash Verma <88872371+Suyash123456789@users.noreply.github.com>
2026-05-10 14:34:18 -0700

    Add: islands-and-treasure - submission-6

---
+++ Data Structures & Algorithms/islands-and-treasure/submission-6.py
@@ -0,0 +1,25 @@
+from collections import deque
+from typing import List
+
+class Solution:
+    def islandsAndTreasure(self, rooms: List[List[int]]) -> None:
+        ROWS, COLS = len(rooms), len(rooms[0])
+        q = deque()
+        INF = 2147483647
+
+        for r in range(ROWS):
+            for c in range(COLS):
+                if rooms[r][c] == 0:
+                    q.append((r, c))
+
+        directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+
+        while q:
+            r, c = q.popleft()
+
+            for dr, dc in directions:
+                nr, nc = r + dr, c + dc
+
+                if 0 <= nr < ROWS and 0 <= nc < COLS and rooms[nr][nc] == INF:
+                    rooms[nr][nc] = rooms[r][c] + 1
+                    q.append((nr, nc))
