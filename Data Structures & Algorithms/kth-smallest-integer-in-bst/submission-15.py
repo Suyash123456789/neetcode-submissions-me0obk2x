229778f5dd505105902bdcf77969a5edd093149f
Suyash Verma <88872371+Suyash123456789@users.noreply.github.com>
2026-07-22 16:46:05 -0700

    Add: kth-smallest-integer-in-bst - submission-15

---
+++ Data Structures & Algorithms/kth-smallest-integer-in-bst/submission-15.py
@@ -0,0 +1,23 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+        n = 0
+        cur = root
+        stack = []
+        while cur or stack:
+            while cur:
+                stack.append(cur)
+                cur = cur.left
+            node = stack.pop()
+            if node:
+                n += 1
+                if n == k:
+                    return node.val
+            cur = node.right
+        return -1
