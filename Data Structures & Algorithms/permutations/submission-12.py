37d5119dc60fe3c3581dcf24e996d2f55e8db4c6
Suyash Verma <88872371+Suyash123456789@users.noreply.github.com>
2026-07-24 13:49:10 -0700

    Add: permutations - submission-12

---
+++ Data Structures & Algorithms/permutations/submission-12.py
@@ -0,0 +1,19 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        self.res = []
+        
+
+        def backtrack(perm, nums, pick):
+            if len(perm) == len(nums):
+                self.res.append(perm.copy())
+                return
+
+            for i in range(len(nums)):
+                if not pick[i]:
+                    perm.append(nums[i])
+                    pick[i] = True
+                    backtrack(perm, nums, pick)
+                    pick[i] = False
+                    perm.pop()
+        backtrack([], nums, [False] * len(nums))
+        return self.res
